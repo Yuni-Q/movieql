3ff8347b6bdabb602259424a09932c6fdf209810
Yuni-Q <carnotcycle2@naver.com>
2018-09-07 23:39:56 +0900

    3th commit

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: "1",
-    name: "Nicolas",
-    age: 18,
-    gender: "female"
+    id: 0,
+    name: "Star Wars -The new one",
+    score: 1
   },
   {
-    id: "2",
-    name: "Jisu",
-    age: 18,
-    gender: "female"
+    id: 1,
+    name: "Avengers -The new one",
+    score: 8
   },
   {
-    id: "3",
-    name: "Japan Guy",
-    age: 18,
-    gender: "female"
+    id: 2,
+    name: "The Godfather I",
+    score: 99
   },
   {
-    id: "4",
-    name: "Daal",
-    age: 18,
-    gender: "female"
-  },
-  {
-    id: "5",
-    name: "JD",
-    age: 18,
-    gender: "female"
-  },
-  {
-    id: "6",
-    name: "moondaddi",
-    age: 18,
-    gender: "female"
-  },
-  {
-    id: "7",
-    name: "flynn",
-    age: 18,
-    gender: "female"
+    id: 3,
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
+  const filteredPeople = people.filter(person => person.id === id);
   return filteredPeople[0];
+};
+
+export const deleteMovie = id => {
+  const cleanMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanMovies.length) {
+    movies = cleanMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
